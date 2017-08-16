58bdad23c413781d221814e86efc1b2ccbb57e42
Ivana Veliskova <iveliskova@weblinc.com>
2017-08-16 18:58:55 -0400

    Adds autoprefixer and updates gulp sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,19 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    autoprefixer = require('autoprefixer'),
+    postcss = require('gulp-postcss');
 
 gulp.task('sass', function() {
-    return gulp.src('./stylesheets/*.scss')
+    return gulp.src('./stylesheets/**.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([autoprefixer({ browsers: ['last 2 versions', '>2%']})]))
         .pipe(gulp.dest('./public/assets/css'));
 });
 
 gulp.task('sass:watch', function() {
-    gulp.watch('./stylesheets/*.scss', ['sass']);
+    gulp.watch('./stylesheets/**/*.scss', ['sass']);
 });
+
+gulp.task('sass:run', ['sass', 'sass:watch']);
